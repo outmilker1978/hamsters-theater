b6d3c0e9977cb6540dc1c36423029636362195ee
outmilker1978 <outmilker@gmail.com>
2026-06-10 17:28:48 +0300

    mobile: sw.js deletes all caches on activate, bump cache bust v=13

--- docs/mobile/sw.js
+++ docs/mobile/sw.js
@@ -1,3 +1,9 @@
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('activate', (e) => {
+  e.waitUntil((async () => {
+    await self.clients.claim();
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => caches.delete(k)));
+  })());
+});
 self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
